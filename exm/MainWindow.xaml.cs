e2302b9db22fd804c7ec16fa3d56a127eb6cf8ef
Dacdako <116169717+Dacdako@users.noreply.github.com>
2023-06-19 14:22:19 +0700

    Добавьте файлы проекта.

---
+++ exm/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace exm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public class muzic
+        {
+            public string name { get; set; }
+            public string janr { get; set; }
+            public string aftor { get; set; }
+        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            basa.SelectedValue = iii.Text;
+        }
+    }
+}
